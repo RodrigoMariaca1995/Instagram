5f1b2d77ba9e649db869a3baaa73a0b4201d2044
RodrigoMariaca1995 <rlmariac@cougarnet.uh.edu>
2021-10-08 13:43:24 -0500

    Update new gif

--- app/src/main/java/com/example/instagram/Post.java
+++ app/src/main/java/com/example/instagram/Post.java
@@ -11,6 +11,7 @@ public class Post extends ParseObject{
     public static final String KEY_DESCRIPTION = "description";
     public static final String KEY_IMAGE = "image";
     public static final String KEY_USER = "user";
+    public static final String KEY_CREATED_AT = "createdAt";
 
     public String getDescription()
     {
@@ -22,7 +23,7 @@ public class Post extends ParseObject{
         put(KEY_DESCRIPTION, description);
     }
 
-    public ParseFile getImage(ParseFile parseFile)
+    public ParseFile getImage()
     {
         return getParseFile(KEY_IMAGE);
     }
